b443270f31cabf24816c13e20b8af891787b4388
Lyndon Francisco <lyndon.francisco@amkor.com>
2026-05-06 10:09:48 +0800

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { supabase } from '/lib/supabaseClients'
+import { supabase } from '@/lib/supabaseClients'
 
 export default function Home() {
   const [tasks, setTasks] = useState<any[]>([])
